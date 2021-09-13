64fa3baeaded08a566c913addc7b41864462b80e
Erez Friemagor <erezfree29@gmail.com>
2021-09-13 20:48:05 +0100

    fetch rockets

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,5 +1,16 @@
-const RocketsPage = () => (
-  <div className="categories" />
-);
+function RocketsPage() {
+  const fetchItems = async () => {
+    const data = await fetch('https://api.spacexdata.com/v3/rockets');
+    const rockets = await data.json();
+    console.log(rockets);
+  };
 
+  fetchItems();
+
+  return (
+    <div className="page">
+      <h1>Fetch</h1>
+    </div>
+  );
+}
 export default RocketsPage;
