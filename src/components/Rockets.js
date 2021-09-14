d0281dcfd22746da54aa380827fbad379f6ebc22
Erez Friemagor <erezfree29@gmail.com>
2021-09-14 17:45:27 +0100

    update reservation ui in rocket description

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -45,14 +45,15 @@ function RocketsPage() {
     dispatch(updateState(rockets));
   }
 
-  function showReservation (id) {
-    if (document.getElementById(id).classList.contains('btn-primary') === false) {
-      document.getElementById(id).style.backgroundColor = 'none';
-      document.getElementById(id).classList.add('btn-primary');
-      document.getElementById(id).innerHTML = 'Reserve Rocket';
+  function showReservation (rocketName, description) {
+    if (document.getElementById(rocketName).classList.contains('btn-primary') === false) {
+      document.getElementById(rocketName).classList.add('btn-primary');
+      document.getElementById(rocketName).innerHTML = 'Reserve Rocket';
+      document.getElementById(description).style.display = 'none';
     } else {
-      document.getElementById(id).classList.remove('btn-primary');
-      document.getElementById(id).innerHTML = 'Cancel Reservation';
+      document.getElementById(rocketName).classList.remove('btn-primary');
+      document.getElementById(rocketName).innerHTML = 'Cancel Reservation';
+      document.getElementById(description).style.display = 'block';
     }
   }
 
@@ -61,17 +62,20 @@ function RocketsPage() {
       {rockets.map((rocket) => (
         <div className="rocket">
           <img src={rocket.image} alt="rocket" width="150px" />
-          <div className="rocket_details">
+          <div className="rocket_details" id={rocket.id + 100}>
             <div>
               <h4>{rocket.rocket_name}</h4>
-              <div className="description">{rocket.description}</div>
+              <div className="description">
+                <div id={rocket.description} style={{ display: 'none' }} className="reserved">reserved</div>
+                <div>{rocket.description}</div>
+              </div>
               <button
                 type="button"
                 className="btn btn-primary"
-                id={rocket.id}
+                id={rocket.rocket_name}
                 onClick={() => {
                   fetchRocket(rocket.id);
-                  showReservation(rocket.id);
+                  showReservation(rocket.rocket_name, rocket.description);
                 }}
               >
                 Reserve Rocket
