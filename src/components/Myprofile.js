1b720ec0610164842a379d1be7dc32b68d66b9bc
Erez Friemagor <erezfree29@gmail.com>
2021-09-15 14:34:38 +0100

    add reserved rockets to my profile

--- src/components/Myprofile.js
+++ src/components/Myprofile.js
@@ -1,7 +1,34 @@
-const MyprofilePage = () => (
-  <div className="categories">
-    <h1>Under Construction</h1>
-  </div>
-);
+/* eslint-disable max-len */
+/* eslint-disable react/jsx-key */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-return-assign */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-unescaped-entities */
+import { useSelector } from 'react-redux';
 
+function MyprofilePage() {
+  const reservedRockets = useSelector((state) => state.rocketsReducer.filter((rocket) => rocket.reserved === true));
+
+  return (
+    <div className="reserved_rockets">
+      <h2>Reserved Rockets</h2>
+      <div>
+
+        {reservedRockets.map((rocket) => (
+          <div className="rocket">
+            <img src={rocket.image[0]} alt="rocket" width="150px" />
+            <div className="rocket_details">
+              <h4><div>{rocket.rocket_name}</div></h4>
+              <div className="description">
+                <div id={rocket.description} style={{ display: 'none' }} className="reserved">reserved</div>
+                <div>{rocket.description}</div>
+              </div>
+            </div>
+          </div>
+        ))}
+
+      </div>
+    </div>
+  );
+}
 export default MyprofilePage;
