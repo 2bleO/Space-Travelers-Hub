4f0beacfbc3a247756ee4ec0fb3d3450aeb57c53
onesis <Onesis.olea@gmail.com>
2021-09-16 07:18:16 -0400

    add joines missions and reserved rockets to my profile as per design

--- src/components/Myprofile.js
+++ src/components/Myprofile.js
@@ -1,7 +1,47 @@
-const MyprofilePage = () => (
-  <div className="categories">
-    <h1>Under Construction</h1>
-  </div>
-);
+/* eslint-disable max-len */
+/* eslint-disable react/jsx-key */
+import { useSelector } from 'react-redux';
+import {
+  Table, Container,
+} from 'react-bootstrap';
 
+function MyprofilePage() {
+  const reservedRockets = useSelector((state) => state.rocketsReducer.filter((rocket) => rocket.reserved === true));
+  const joinedMissions = useSelector((state) => state.missionReducer.filter((mission) => mission.reserved === true));
+
+  return (
+    <>
+      <Container fluid className="d-flex">
+        <Table className="col mx-2">
+          <thead>
+            <tr>
+              <th scope="col">My Missions</th>
+            </tr>
+          </thead>
+          <tbody>
+            { joinedMissions.map((mission) => (
+              <tr>
+                <td>{mission.mission_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+        <Table className="col mx-2">
+          <thead>
+            <tr>
+              <th scope="col">My Rockets</th>
+            </tr>
+          </thead>
+          <tbody>
+            { reservedRockets.map((rocket) => (
+              <tr>
+                <td>{rocket.rocket_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
+    </>
+  );
+}
 export default MyprofilePage;
